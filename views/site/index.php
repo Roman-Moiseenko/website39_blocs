77c58753c8d0df46856a727a67172bac2da0b1ac
Roman-Moiseenko <60714951+Roman-Moiseenko@users.noreply.github.com>
2021-08-03 21:44:26 +0200

    Text+

--- views/site/index.php
+++ views/site/index.php
@@ -10,7 +10,7 @@ $this->registerMetaTag(['name' => 'og:description', 'content' => $description]);
 ?>
 <div class="item-responsive item-2-80by1">
     <div class="content-item">
-        <img data-src="/images/main_land.jpg" class="lazyload" alt="На отдых в Калининградскую область" width="100%">
+        <img data-src="/images/main_land.jpg" class="lazyload" alt="Заказать сайт в Калининграде" width="100%">
         <div class="container">
             <div class="carousel-caption">
                 <h1 class="landing-h1">Заказать сайт в Калининграде</h1>
@@ -22,15 +22,15 @@ $this->registerMetaTag(['name' => 'og:description', 'content' => $description]);
     <div class="card my-4 p-2" style="border-radius: 40px;">
         <div class="card-body">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-6 block-main">
                     <h2>Разработка сайта компании</h2>
-                    <ul class="list-landing">
                         <h3>Функционал под специфику Вашего бизнеса</h3>
                         <h3>Удобный и интуитивно понятный интерфейс</h3>
                         <h3>Мобильная версия Вашего сайта</h3>
-                    </ul>
                 </div>
-                <div class="col-md-6">IMG</div>
+                <div class="col-md-6">
+                    <img data-src="/images/longpage_01.jpg" class="lazyload" alt="Разработка сайта компании" width="100%">
+                </div>
             </div>
         </div>
     </div>
@@ -38,13 +38,11 @@ $this->registerMetaTag(['name' => 'og:description', 'content' => $description]);
         <div class="card-body">
             <div class="row">
                 <div class="col-md-6">IMG</div>
-                <div class="col-md-6">
+                <div class="col-md-6 block-main">
                     <h2>Разработка CRM-систем</h2>
-                    <ul class="list-landing">
-                        <li>Создание закрытых CRM-систем</li>
-                        <li>Функционал любой сложности</li>
-                        <li>Срок разработки от 2 до 6 месяцев</li>
-                    </ul>
+                        <h3>Создание закрытых CRM-систем</h3>
+                        <h3>Функционал любой сложности</h3>
+                        <h3>Срок разработки от 2 до 6 месяцев</h3>
                 </div>
             </div>
         </div>
@@ -52,12 +50,10 @@ $this->registerMetaTag(['name' => 'og:description', 'content' => $description]);
     <div class="card my-4 p-2" style="border-radius: 40px;">
         <div class="card-body">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-6 block-main">
                     <h2>Подключение онлайн-платежей</h2>
-                    <ul class="list-landing">
-                        <li>Подключим Ю-Кассу</li>
-                        <li>Настроим отчетность интернет платежей</li>
-                    </ul>
+                        <h3>Подключим Ю-Кассу</h3>
+                        <h3>Настроим отчетность интернет платежей</h3>
                 </div>
                 <div class="col-md-6">IMG</div>
             </div>
@@ -67,9 +63,12 @@ $this->registerMetaTag(['name' => 'og:description', 'content' => $description]);
         <div class="card-body">
             <div class="row">
                 <div class="col-md-6">IMG</div>
-                <div class="col-md-6">
-                    <h2>Расширенный Функционал сайта</h2>
-                    СМС и другие функции
+                <div class="col-md-6 block-main">
+                    <h2>Расширенный функционал сайта</h2>
+                    <h3>СМС-уведомления</h3>
+                    <h3>Чат на сайте</h3>
+                    <h3>Форум и комментарии к страницам</h3>
+                    <h3>Мультиязычность</h3>
                 </div>
             </div>
         </div>
@@ -77,9 +76,12 @@ $this->registerMetaTag(['name' => 'og:description', 'content' => $description]);
     <div class="card my-4 p-2" style="border-radius: 40px;">
         <div class="card-body">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-6 block-main">
                     <h2>Яндекс карта, геолокация</h2>
-
+                    <h3>Подключение API Яндекс карты</h3>
+                    <h3>Поиск по карте</h3>
+                    <h3>Построение маршрутов</h3>
+                    <h3>Добавление собственных областей</h3>
                 </div>
                 <div class="col-md-6">IMG</div>
             </div>
@@ -89,9 +91,13 @@ $this->registerMetaTag(['name' => 'og:description', 'content' => $description]);
         <div class="card-body">
             <div class="row">
                 <div class="col-md-6">IMG</div>
-                <div class="col-md-6">
+                <div class="col-md-6 block-main">
                     <h2>Оптимизация сайта под поисковики</h2>
-                    SEO-оптимизация сайта, Гугл Сеарч, Схема.орг, ЧПУ
+                    <h3>SEO-оптимизация сайта</h3>
+                    <h3>Настройка Заголовков и Описания</h3>
+                    <h3>ЧПУ ссылки</h3>
+                    <h3>Подключение мета-тегов schema.org</h3>
+                    <h3>Оптимизация быстрой загрузки PageSpeed Insights</h3>
                 </div>
             </div>
         </div>
