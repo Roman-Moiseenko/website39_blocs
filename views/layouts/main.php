42c40f946ec0230510d17df9c28daab982091ca4
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2021-08-22 14:31:04 +0200

    Patch+++

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,6 +17,7 @@ AppAsset::register($this);
 <html lang="<?= Yii::$app->language ?>" class="h-100">
 <head>
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
